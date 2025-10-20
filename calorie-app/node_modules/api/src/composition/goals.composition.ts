e529a2bf0daa6c95b8dade27a539d7edd3d9c011
Rodrigo Hernández <rhernandezmartin66@gmail.com>
2025-10-20 08:50:15 +0200

    b

--- calorie-app/node_modules/api/src/composition/goals.composition.ts
+++
@@ -1,7 +0,0 @@
-import { GoalsService } from "../services/goals.service";
-import { GoalsAdapter } from "../repositories/impl/goals.adapter";
-
-export function buildGoalsService() {
-const repo = new GoalsAdapter();
-return new GoalsService(repo);
-}
