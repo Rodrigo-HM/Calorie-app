af9f594020a9a68847b7636971d581d8ad2235c3
Rodrigo Hernández <rhernandezmartin66@gmail.com>
2025-10-20 15:47:41 +0200

    b

--- calorie-app/node_modules/api/src/module/entries/domain/events/DomainEvent.ts
+++
@@ -1,6 +0,0 @@
-export type DomainEvent = {
-  eventName: string;
-  aggregateId: string;
-  occurredOn: Date;
-  payload: Record<string, unknown>;
-};
--- calorie-app/node_modules/api/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-export default defineConfig({
-test: {
-environment: "node", //sin navegador
-globals: true, //para no tener que importar describe, it, expect...
-include: ["tests/**/*.{test,spec}.ts"], //dónde están los tests
-setupFiles: ["./tests/setup.ts"], //lo que se ejecuta antes de cada test
-},
-});
